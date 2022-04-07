7e40399f6bb3d000bb971dad0ed201e4dc5f4c3d
Muhammad-yogi <yogikgaek@gmail.com>
2022-04-08 00:27:35 +0800

    array pada data body table telah di ubah

--- script.js
+++ script.js
@@ -1,46 +1,53 @@
 // DOM H1
-const h1 = document.createElement(`h1`);
-document.getElementById(`h1`).appendChild(h1);
-h1.innerHTML = `Membuat mini library From javascript`;
-h1.style.textAlign = `center`; h1.style.fontFamily = `fantasy`;
+const funcH1 = () => {
+    const h1 = document.createElement(`h1`);
+    document.getElementById(`h1`).appendChild(h1);
+    h1.innerHTML = `Membuat mini library From javascript`;
+    h1.style.textAlign = `center`; h1.style.fontFamily = `fantasy`;
+};
 
 // DOM button
-const button = document.createElement(`button`);
-document.querySelector(`#button`).appendChild(button);
-button.innerHTML = `Klik untuk menampilkan table`;
-button.style.display = `inline-Block`; button.style.padding = `10px 20px`; 
-button.style.position = `absolute`; button.style.top = `10%`; button.style.left= `42%`;
+const funcButoon = () => {
+    const button = document.createElement(`button`);
+    document.querySelector(`#button`).appendChild(button);
+    button.innerHTML = `Klik untuk menampilkan table`;
+    button.style.display = `inline-Block`; button.style.padding = `10px 20px`;
+    button.style.position = `absolute`; button.style.top = `10%`; button.style.left = `42%`;
+};
+funcH1();
+funcButoon();
 
 // Use class dan method
 class Gaek {
-    constructor (head, head1, head2, dataBody){
-        this.headx      = head;
-        this.head1x     = head1;
-        this.head2x     = head2;
-        this.dataBodyx  = dataBody;
+    constructor(head, head1, head2, dataBody) {
+        this.headx = head;
+        this.head1x = head1;
+        this.head2x = head2;
+        this.dataBodyx = dataBody;
         this.thead = document.createElement(`thead`);
         this.tbody = document.createElement(`tbody`);
         this.row = document.createElement(`tr`);
         this.row1 = document.createElement(`tr`);
     }
-    table(){
+    table() {
         let myTable = document.createElement(`table`);
         let myThead = this.thead;
         let myTbody = this.tbody;
-        let myRow   = this.row;
-        let myRow1  = this.row1;
+        let myRow = this.row;
+        let myRow1 = this.row1;
 
         document.querySelector(`#table`).appendChild(myTable);
 
         myTable.setAttribute(`class`, `table table-bordered border-dark`);
-        
-        myTable.style.position = `absolute`; myTable.style.left = `30%`; myTable.style.top = `20%`; myTable.style.width = `40%`;
+
+        myTable.style.position = `absolute`; myTable.style.left = `25%`; myTable.style.top = `20%`; 
+        myTable.style.width = `50%`;
         myTable.appendChild(myThead);
         myThead.appendChild(myRow);
         myThead.appendChild(myRow1);
         myTable.appendChild(myTbody);
     };
-    header(){
+    header() {
         const th = this.headx.head
         th.forEach(text => {
             let headBulan = document.createElement(`th`);
@@ -54,7 +61,7 @@ class Gaek {
             this.row.appendChild(headBulan);
         });
     }
-    header1(){
+    header1() {
         const th1 = this.headx.head1;
         th1.forEach(text1 => {
             let headHasilpanen = document.createElement(`th`);
@@ -68,7 +75,7 @@ class Gaek {
             this.row.appendChild(headHasilpanen);
         })
     }
-    header2(){
+    header2() {
         const th2 = this.headx.head2;
         th2.forEach(text2 => {
             let headPadi = document.createElement(`th`);
@@ -79,29 +86,29 @@ class Gaek {
             this.row1.appendChild(headPadi);
         })
     }
-    data(){
-        const dataB = this.headx.dataBody;
-        dataB.forEach(textBody => {
-            let dataBody = `<tr> 
-            <td>${textBody[0]}</td>
-            <td>${textBody[1]}</td>
-            <td>${textBody[2]}</td>
-            <td>${textBody[3]}</td>
-            </tr>`;
-            this.tbody.innerHTML += dataBody;
-        })
+    data() {
+        const dataB = this.headx.dataBody
+            dataB.forEach(d => {
+                let db = `<tr>`;
+                d.forEach(text3 => {
+                    db += `<td>${text3}</td>`;
+                })
+                db += `</tr>`
+                this.tbody.innerHTML += db
+            });   
     }
 };
 
 // use addevent
-button.addEventListener(`click`, function() {
+const addFunc = () => {
+    button.addEventListener(`click`, function () {
         gaek.table();
         gaek.header();
         gaek.header1();
         gaek.header2();
         gaek.data();
-})
-
+    })
+};
 const gaek = new Gaek({
     head: [`Bulan`],
     head1: [`Hasil panen`],
@@ -121,3 +128,6 @@ const gaek = new Gaek({
         [`Desember`, `345 Kg`, `300 Kg`, `600 Kg`]
     ]
 });
+addFunc();
+
+// export {funcH1, funcButoon, gaek}
